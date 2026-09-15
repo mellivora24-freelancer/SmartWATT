46b9af809ae9f1f02bfe55f2e8be88126a0933eb
mellivora24 <thanh139ptit@gmail.com>
2026-09-15 21:16:44 +0700

    update

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "SmartWatt development environment";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
